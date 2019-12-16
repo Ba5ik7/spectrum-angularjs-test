5029c09cefec02faf422f74d9b0af010b8424b64
Wesley Dusell <wesleydusell@Wesleys-MacBook-Air.local>
2019-12-15 22:02:39 -0500

    Create a home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './routes/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, data: {animation: 'home'} },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
