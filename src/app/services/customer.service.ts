2cb786fc2a7c29cb6f0cd816f82fcbdf4c93c474
Wesley Dusell <wesleydusell@Wesleys-MacBook-Air.local>
2019-12-15 22:50:09 -0500

    Create a simple CRUD customer service

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Customer } from '../interfaces/customer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  customers: Array<Customer>;
+
+  constructor() { }
+
+  create() {
+
+  }
+
+  read() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
